50acafc66bd66c2368949c4791f1fb8386332e5c
amjoshi <amjoshi@cpp.edu>
2017-02-18 01:33:48 -0800

    added partial pseudoCode to addIdentifier method()

--- DFSATrie.java
+++ DFSATrie.java
@@ -6,59 +6,49 @@ import java.util.TreeMap;
 
 
 public class DFSATrie {
-
 	/**
-	 * @param args
-	 */
-	/*
 	 * switch array is the array list that contains all the switch start 
 	 */
 	static int [] switchArray = new int[54];
-	/*
+	/**
 	 * by using the TreeMap I am mapping the letters, underscore and dollar sign to indexes to the switch array 
 	 */
 	static TreeMap<String, Integer> alphabetTable = new TreeMap <String, Integer>();
-	/*
+	/**
 	 * the symbol and next array contain the information for all the 
 	 */
 	static ArrayList<String>symbol = new ArrayList<String>();
 	static ArrayList<Integer>next = new ArrayList<Integer>();
-	public static void main(String[] args) {
-		String fileName= "Proj2_Input1.txt";
-		try{
-			intializeArray(); 
-			//			
-			/*
-			 * start file read using a buffered reader 
-			 */
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-
-
+	/**
+	 * a way of storing all known identifiers to allow for easy lookup / search 
+	 */
+	ArrayList<String> identifiers = new ArrayList<String>();
+	/**
+	 * @param String s 
+	 * @return true if we have already found this identifier in the files
+	 */
+	public boolean hasIdentifier(String s){
+		if (identifiers.contains(s)){
+			return true;
 		}
-		catch(Exception e){
-			System.out.println("ERROR IN OUTER TRY");
-		}
-
+		return false;
 	}
-
-	private static void intializeArray() {
-		/*
-		 * initalize switch table 
-		 */
-		createSwitchTable(); 
-
-
-	}
+	/**
+	 * @param char s 
+	 * adds the char value to the symbol array and the next array 
+	 */
 	public static void addSymbol(char s){
 		symbol.add(s+"");
 		next.add(-1);
 	}
+	/**
+	 * Prints the next and symbol array with proper format
+	 */
 	private static void printSymbolNextArray() {
 		int size = symbol.size(); 
 		int tempSymbol = 0; 
 		int tempNext=0;
-		
+
 		while(size>0 && tempSymbol<size && tempNext<size){
 			if (tempSymbol%20==0){
 				System.out.print("\nSymbol:");
@@ -85,7 +75,9 @@ public class DFSATrie {
 
 
 	}
-
+	/**
+	 * initializes the switch table with -1 and hash table to map the letters, and the _ and $ to indexes
+	 */
 	private static void createSwitchTable(){
 		/*
 		 * create the array Lists to store the switch and create a hash table to store the matching indexes for each alphabet  
@@ -108,6 +100,9 @@ public class DFSATrie {
 		i++;
 		alphabetTable.put("$", i);
 	}
+	/**
+	 * prints the switch table with proper formatting 
+	 */
 	private static void printSwitch() {
 		System.out.printf("%8s"," ");
 		for (char c = 'A'; c <='T'; c++){
@@ -150,6 +145,56 @@ public class DFSATrie {
 		System.out.printf("%5s", switchArray[alphabetTable.get("_")]);
 		System.out.printf("%5s", switchArray[alphabetTable.get("$")]);
 		System.out.println();
+
+	}
+	
+	private void addIdentifier(String s, char endSymbol){
+		/**
+		 * if hasIdentifier(s) == false then we have not dealt with this identifier 
+		 * Step 1: go to switch Table and check if switch[alphabetTable.get(s.getCharAt(0)] == -1 
+		 * Step 2 a: if -1 then we haven't had an identifier that starts with this char
+		 * Step 3: then change switch[alphabetTable.get(s.getCharAt(0)] to symbol.length
+		 * Step 4: add the rest of the identifier into the symbol using the addSymbol() until the end 
+		 * Step 4a: and add either a "*" if it is a reserved word 
+		 * Step 4b: or add a "?" 
+		 * Step 5: add to identifiers List 
+		 * 
+		 * Step 2b: if != -1 then we have found an identifier that starts with that character 
+		 * Step 3: go to the character at the index given by symbol.get(switch[alphabetTable.get(s.getCharAt(0)])
+		 * Step 4: if the characters are the same, then continue parsing through the symbol table 
+		 * 
+		 * Step 4a: until you hit either a character that is different 
+		 * Step 5a: if you have reached a character that is different the set the next of that symbol to symbol.length 
+		 * Step 6: then add the remaining characters to the symbol using the addSymbol() until the end 
+		 * Step 7a: and add either a "*" if it is a reserved word 
+		 * Step 7b: or add a "?" 
+		 * 
+		 * Step 4b: or you have finished all the characters within the identifier but there is no end for the identifier you are currently following 
+		 * Step 5: Set the next to the symbol you are currently checking to symbol.length 
+		 * Step 6: add either a * or a ? to the symbol list
+		 * Step 7: add to identifiers List 
+		 */
+		
+	}
+	public static void main(String[] args) {
+		String fileName= "Proj2_Input1.txt";
+		try{
+			/*
+			 * initalize switch table 
+			 */
+			createSwitchTable(); 
+			//			
+			/*
+			 * start file read using a buffered reader 
+			 */
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+
+
+		}
+		catch(Exception e){
+			System.out.println("ERROR IN OUTER TRY");
+		}
 
 	}
 
